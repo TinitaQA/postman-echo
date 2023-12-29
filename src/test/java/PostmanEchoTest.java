9a137f165f3abf89c9f4ced00e1548b4df247f53
Tinita <tino4ka.87@mail.ru>
2023-12-29 19:38:40 +0300

    Falling test fixed

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -19,7 +19,7 @@ public class PostmanEchoTest {
         // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("првет"))
+                .body("data", equalTo("привет"))
         ;
     }
 }
