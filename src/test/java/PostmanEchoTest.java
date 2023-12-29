63d892fe51d833a4f5421d925f9eb1c5f8778cc7
Tinita <tino4ka.87@mail.ru>
2023-12-29 19:20:57 +0300

    Initial commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class PostmanEchoTest {
+
+    @Test
+    public void Test() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("привет") // отправляемые данные (заголовки и query можно выставлять аналогично)
+        // Выполняемые действия
+                .when()
+                .post("/post")
+        // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("привет"))
+        ;
+    }
+}
